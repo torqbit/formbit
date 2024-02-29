f1c45c36024bd0eecc119fbfdcd7fbc0962e995d
Shadab Alam <shadab19it@gmail.com>
2024-02-29 13:40:54 +0530

    updated title

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
-  title: 'formbit',
+  title: 'Formbit - A website template for SaaS',
   description:
     'Next.JS + TailwindCSS + HeadlessUI powered website template for your next SaaS marketing website',
   url: 'https://formbit.vercel.app/',
