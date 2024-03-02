2f732c11872c720784a95e32d8da970a50694017
mehrab-alam <mehrab2000alam@gmail.com>
2024-03-02 22:11:06 +0530

    fixed build bug

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -3,14 +3,26 @@
 import Footer from '@/components/Footer/Footer';
 import Header from '@/components/Header/Header';
 import HeroPrice from '@/components/Pricing/page/HeroPrice';
+import { useState } from 'react';
 
 const PricingPage = () => {
+  const [menuActive, setMenuActive] = useState({
+    active: false,
+    menu: '',
+  });
+  const onMenuActive = (value: boolean, name: string) => {
+    setMenuActive({ active: value, menu: name });
+  };
   return (
-    <>
-      <Header />
+    <section
+      onClick={() => {
+        setMenuActive({ active: false, menu: '' });
+      }}
+    >
+      <Header menuActive={menuActive} onMenuActive={onMenuActive} />
       <HeroPrice />
       <Footer />
-    </>
+    </section>
   );
 };
 
